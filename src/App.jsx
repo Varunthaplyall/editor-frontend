4999becbf98f83f6fc5b54688a203b8a7758201a
varunt-rapidquest <Varunthapliyal70@gmail.com>
2025-05-17 15:24:17 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ function App() {
 
   return (
     <div style={{ backgroundColor: '#1e1e1e', color: '#ffffff', minHeight: '100vh', padding: '1rem', overflow: 'auto' }}>
-    <h2 style={{ color: '#22d3ee', fontSize: '1.5rem', marginBottom: '1rem', fontWeight: 600, textAlign: 'center', letterSpacing: '2px', textShadow: '0 0 8px #22d3ee', fontFamily: "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif" }}>
+    <h2 style={{ color: '#22d3ee', fontSize: '1.8rem', marginBottom: '1rem', fontWeight: 600, textAlign: 'center', letterSpacing: '2px', textShadow: '0 0 8px #22d3ee', fontFamily: "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif" }}>
       Python Code Runner </h2>
 
       <div style={{ marginBottom: '1rem' }}>
